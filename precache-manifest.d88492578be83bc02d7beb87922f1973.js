20f117bda075ea1310c672ad9950542d48c302f1
yusrilibnu <yusril.ibnu@gmail.com>
2020-07-26 00:46:07 +0700

    Updates

---
+++ precache-manifest.d88492578be83bc02d7beb87922f1973.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d02b3646dce8da04306861b6eb87fdc",
+    "url": "/yusril_portfolio/index.html"
+  },
+  {
+    "revision": "f8f6f9e481381322bca9",
+    "url": "/yusril_portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "ef5588a02de255a54cb2",
+    "url": "/yusril_portfolio/static/js/2.ca9fa377.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/yusril_portfolio/static/js/2.ca9fa377.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8f6f9e481381322bca9",
+    "url": "/yusril_portfolio/static/js/main.77ef9363.chunk.js"
+  },
+  {
+    "revision": "0b4d43aa8b7feb04d18a",
+    "url": "/yusril_portfolio/static/js/runtime-main.963d110a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/yusril_portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
